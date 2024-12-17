bb7cfca9ffdf00a19ee20ab7d5507c79b46e3f3d
Metronon <bumpercar07@gmail.com>
2024-12-17 11:53:18 +0900

    add PostWriteForm class

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -1,33 +1,57 @@
 package com.ll.tem.domain.post.post.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
+@Validated
 public class PostController {
+    private String getFormHtml(String errorMessage, String title, String content) {
+        return """
+                <div>%s</div>
+                <form method="POST">
+                    <input type="text" name="title" placeholder="제목" value="%s">
+                    <br>
+                    <textarea name="content" placeholder="내용">%s</textarea>
+                    <br>
+                    <button type="submit">글쓰기</button>
+                </form>
+                """.formatted(errorMessage, title, content);
+    }
 
     @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
-        return """
-                <form method="POST">
-                    <input type="text" name="title" placeholder="제목">
-                    <textarea name="content" placeholder="내용"></textarea>
-                    <button type="submit">글쓰기</button>
-                </form>
-                """;
+        return getFormHtml("", "", "");
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @ToString
+    public static class PostWriteForm {
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Length(min = 5, message = "제목을 5자 이상 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Length(min = 10, message = "내용을 10자 이상 입력해주세요.")
+        private String content;
     }
 
     @PostMapping("/write")
     @ResponseBody
     public String write(
-            String title,
-            String content
+            @ModelAttribute PostWriteForm form
     ) {
+        System.out.println("form = " + form);
+
         return """
                 <h1>글쓰기 완료</h1>
                 
@@ -35,6 +59,6 @@ public class PostController {
                     <h2>%s</h2>
                     <p>%s</p>
                 </div>
-                """.formatted(title, content);
+                """.formatted(form.getTitle(), form.getContent());
     }
 }
